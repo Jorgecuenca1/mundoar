7168864baeecf6c266f075da7a1d99e6d12c1947
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2024-05-27 23:17:21 -0500

    Kk

--- myarapp/urls.py
+++ myarapp/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import ARSceneView, indexView
+from .views import (
+    ARSceneView, indexView, whychooseView, contactView, aboutView,
+    itserviceView, managedserviceView, industriesView, businessView, servicedetailView
+)
 
 urlpatterns = [
     path('ar', ARSceneView.as_view(), name='ar_scene'),
+    path('nosotros', aboutView.as_view(), name='about'),
+    path('contacto', contactView.as_view(), name='contact'),
+    path('eligenos', whychooseView.as_view(), name='whychoose'),
     path('', indexView.as_view(), name='index'),
+    path('it-services', itserviceView.as_view(), name='it-services'),
+    path('managed-it-service', managedserviceView.as_view(), name='managed-it-service'),
+    path('industries', industriesView.as_view(), name='industries'),
+    path('business-solution', businessView.as_view(), name='business-solution'),
+    path('it-services-details', servicedetailView.as_view(), name='it-services-details'),
 ]
--- myarapp/views.py
+++ myarapp/views.py
@@ -15,3 +15,49 @@ class indexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+class aboutView(View):
+    template_name = 'mintech/about-us-01.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class contactView(View):
+    template_name = 'mintech/contact-us.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class whychooseView(View):
+    template_name = 'mintech/why-choose-us.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class itserviceView(View):
+    template_name = 'mintech/it-services.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+class managedserviceView(View):
+    template_name = 'mintech/managed-it-service.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class industriesView(View):
+    template_name = 'mintech/industries.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+class businessView(View):
+    template_name = 'mintech/business-solution.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class servicedetailView(View):
+    template_name = 'mintech/it-services-details.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
